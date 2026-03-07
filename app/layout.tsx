7159143a19b299e70779f7d11f211a9cdbd5dbec
VictordaSilvaf <victordasilvafernandes@gmail.com>
2026-03-07 15:50:58 -0300

    feat: adicionar suporte ao Google Tag Manager no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -25,7 +26,33 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning className="bg-primary">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5F2CLH95');
+            `,
+          }}
+        />
+      </head>
       <body className={` ${poppins.variable} antialiased bg-primary`}>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5F2CLH95"
+            title="Google Tag Manager"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>`,
+          }}
+        />
         {children}
         <Analytics />
       </body>
